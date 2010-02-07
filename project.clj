6dfbca8ba77454f7dae1469cffcdf9d0ffeb68cc
Richard Newman <rnewman@twinql.com>
2010-02-06 19:28:30 -0800

    Pulling sessions changes in from weavejester. Clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject ring "0.1.1-SNAPSHOT"
+(defproject org.clojars.rnewman/ring "0.2.1-sessions"
   :description "A Clojure web applications library."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [org.mortbay.jetty/jetty "6.1.14"]
                  [org.mortbay.jetty/jetty-util "6.1.14"]
                  [org.mortbay.jetty/servlet-api-2.5 "6.1.14"]
